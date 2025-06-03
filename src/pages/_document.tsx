83b0d6bc46d79513e02ac295708ceb656fe33722
Michel Manarin <michelmanarin@gmail.com>
2025-06-03 09:17:47 -0300

    feat adicionando loadster

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -50,13 +50,15 @@ export default function Document() {
         />
 
         {/* Neurolead Script
+        */}
+
         <script
           dangerouslySetInnerHTML={{
             __html: `
               (function(a,b,c,d){try{var e=b.head||b.getElementsByTagName("head")[0];var f=b.createElement("script");f.setAttribute("src",c);f.setAttribute("charset","UTF-8");f.defer=true;a.neuroleadId=d;e.appendChild(f)}catch(g){}})(window,document,"https://cdn.leadster.com.br/neurolead/neurolead.min.js","2SSMRMfri8tVCzqncNZStzSaw")
             `
           }}
-        />*/}
+        />
         
 
         {/* Schema.org markup para Advogado */}
